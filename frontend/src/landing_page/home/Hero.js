2e13c8ba31cc66d00c734fedce5b0e7f74bfc675
Vedant.ccbp <vedantsonawane93@gmail.com>
2026-08-25 16:56:19 +0530

    heo

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className="container">
+            <div className="row">
+                <img src="media\images\homeHero.png" alt="Hero image" />
+                <h1>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button>Sign up for free</button>
+            </div>
+        </div>
      );
 }
 
